efc7be932eef6cee343157ae31d9a24876088afc
Syndacks <math.milliken@gmail.com>
2017-10-15 21:52:43 -0400

    Add standard_deviation function

--- week1/homework_1.1.py
+++ week1/homework_1.1.py
@@ -6,11 +6,11 @@ CITIES = { 'Los Angeles': 'losangeles',
  'New York': 'newyork' }
 
 def main():
-    city = validate_city()
-    means = calculate_means(city)
+    city = _validate_city()
+    means = _calculate_means(city)
 
 
-def validate_city():
+def _validate_city():
     city = sys.argv[1]
     if city in CITIES:
         print "Input city: ", city
@@ -19,7 +19,7 @@ def validate_city():
         print "I'm sorry but I don't recognize the city you entered."
 
 
-def calculate_means(city):
+def _calculate_means(city):
     # city weather data file locations
     FILENAME_TEMPLATE = 'weather_%s.csv' % CITIES[city]
 
@@ -50,9 +50,35 @@ def calculate_means(city):
     maximum_temp = max(means)
     average_temp = sum_of_means / count_of_means
 
+    # find standard_deviation
+    sd = _calculate_standard_deviation(means, average_temp)
+
     print "minimum_temp: ", minimum_temp
     print "maximum_temp: ", maximum_temp
     print "average_temp: ", average_temp
+    print "standard_deviation: ", sd
+
+
+def _calculate_standard_deviation(data, mean_of_list):
+    print "data: ", data
+    print "mean_of_list: ", mean_of_list
+
+    for item in data:
+        print item
+
+    sum_of_differences_squared = 0
+    count = 0
+
+    for item in data:
+        difference = abs(int(item) - mean_of_list)
+        sum_of_differences_squared += difference ** 2
+        count += 1
+
+    # calcualte the variance
+    variance = sum_of_differences_squared / count
+    sd = variance ** 0.5
+
+    return sd
 
 
 main()
