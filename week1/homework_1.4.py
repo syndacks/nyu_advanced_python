72f76f9e9363584152704447655822622d9c2fb9
Syndacks <math.milliken@gmail.com>
2017-10-17 22:32:47 -0400

    Working draft of 1.4

--- week1/homework_1.4.py
+++ week1/homework_1.4.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 """
 (Extra credit, from Introduction to Python) Find prime numbers in a range of numbers.
 Ask the user for an integer, and display all prime numbers (i.e., numbers that have no
@@ -12,10 +14,20 @@ Please enter max integer: 5
 $ pyt
 """
 
+
 def find_primes_in_range(user_input):
-    my_range = (1, user_input)
-    for i, term in my_range:
-        print "i: ", i
-        print "term: ", term
+    multiples = range(2,10)
+    print "multiples: ", multiples
+    my_range = range(2, user_input + 1)
+    for term in my_range:
+        divisors_of_term = []
+        for multiple in multiples:
+            divisor = int(term) / int(multiple)
+            print str(term) + "/" + str(multiple) + " = "
+            print "divisor: ", divisor
+            divisors_of_term.append(divisor)
+        for value in divisors_of_term:
+            if(value > 1):
+                print "value: " + str(value) + "---" + " is greater than 1"
 
 find_primes_in_range(10)
