aedd132efedd48d61cd17c1fcd8c8b545336a043
syndacks <dacks.milliken@slader.com>
2017-10-19 13:07:32 -0400

    Refactor the calculate stats function

--- week1/homework_1.1.py
+++ week1/homework_1.1.py
@@ -7,42 +7,46 @@ CITIES = { 'Los Angeles': 'losangeles',
 
 
 def main():
-    city = _validate_city()
-    means = calculate_means(city)
+    city = validate_city()
+    city_data = open_file(city)
+    calculate_stats(city_data)
 
 
-def _validate_city():
+def validate_city():
     city = sys.argv[1]
     if city in CITIES:
+        print "Summary of 2016 weather data (in degrees F) for ", city
         return city
     else:
         print "I'm sorry but I don't recognize the city you entered."
 
 
-def calculate_means(city):
+def open_file(city):
     # city weather data file locations
-    FILENAME_TEMPLATE = 'weather_%s.csv' % CITIES[city]
+    filename_template = 'weather_%s.csv' % CITIES[city]
 
-    fh = open(FILENAME_TEMPLATE)
+    fh = open(filename_template)
     # skip the first line (csv header)
     lines = fh.readlines()[1:]
+    fh.close()
 
-    # temp variables to be used while looipng
+    return lines
+
+
+def calculate_stats(city_data):
+    # temp variables to be used while looping
     sum_of_means = 0
     count_of_means = 0
     means = []
 
-    # loop over each line
-    for line in lines:
+    # loop over each line in city_data
+    for line in city_data:
         items = line.split(',')
         # the mean is the third item a string
         mean = items[2]
-        # add each mean to the array
         means.append(mean)
         sum_of_means += int(mean)
         count_of_means += 1
-    # close the file
-    fh.close()
 
     minimum_temp = min(means)
     maximum_temp = max(means)
@@ -51,7 +55,6 @@ def calculate_means(city):
     # find standard_deviation
     sd = _calculate_standard_deviation(means, average_temp)
 
-    print "Summary of 2016 weather data (in degrees F) for ", city
     print "average_temp: ", average_temp
     print "minimum_temp: ", minimum_temp
     print "maximum_temp: ", maximum_temp
@@ -67,7 +70,7 @@ def _calculate_standard_deviation(data, mean_of_list):
         sum_of_differences_squared += difference ** 2
         count += 1
 
-    # calcualte the variance
+    # calculate the variance
     variance = sum_of_differences_squared / count
     sd = variance ** 0.5
 
