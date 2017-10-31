bcb3042eac58050186f5d28f81a911aa16e3ccff
Syndacks <math.milliken@gmail.com>
2017-10-30 22:38:06 -0400

    First commit for week3 homework

---
+++ week3/3-1_datetime.py
@@ -0,0 +1,13 @@
+import date_simple as ds
+from datetime import datetime
+
+"""The functions we'll create convert strings to dates, or dates to strings. Each
+function either returns a datetime.date object (noted below as dt) or returns a
+string that represents a date. """
+
+# dt = ds.get_date('2016-05-05')
+# dt = ds.get_date('5/5/2016')
+# dt = ds.get_date('5-May-16')
+# dt = ds.get_date()
+
+print dt
