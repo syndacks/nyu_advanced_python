f14470af4d39ef570b0eb3a4c322d5648fc0a2d7
syndacks <dacks.milliken@slader.com>
2017-11-05 16:40:52 -0500

    Add format_string method

--- week3/date_simple.py
+++ week3/date_simple.py
@@ -37,3 +37,18 @@ def add_week(date_obj, *argv):
         week_delta = time_delta * 7
         new_date_obj = date_obj + week_delta
     return new_date_obj
+
+
+def format_date(date_obj, *out_format):
+    """format_date(): takes a date object and an optional string and returns a string"""
+    if out_format:
+        format_dict = {
+            'MM/DD/YYYY': '%m/%d/%Y',
+            'DD-Mon-YY':'%d/%a/%y'
+            }
+        date_pattern = format_dict[str(*out_format)]
+    else:
+        date_pattern = '%d/%m/%Y'
+
+    formated_string = date_obj.strftime(date_pattern)
+    return formated_string
