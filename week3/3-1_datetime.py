06a0eb02bfb87d0749daaebfb1bb48be0d5cbe36
syndacks <dacks.milliken@slader.com>
2017-11-05 16:09:48 -0500

    Add add_day and add_week functions

--- week3/3-1_datetime.py
+++ week3/3-1_datetime.py
@@ -6,8 +6,16 @@ function either returns a datetime.date object (noted below as dt) or returns a
 string that represents a date. """
 
 # dt = ds.get_date('2016-05-05')
-# dt = ds.get_date('5/5/2016')
+dt = ds.get_date('5/5/2016')
 # dt = ds.get_date('5-May-16')
 # dt = ds.get_date()
 
-print dt
+print "dt", dt
+
+# dt2 = ds.add_day(dt, 3)
+# dt2 = ds.add_day(dt)
+# print "dt2", dt2
+
+# dt3_add_week = ds.add_week(dt)
+dt3_add_week = ds.add_week(dt,2)
+print "dt3_add_week", dt3_add_week
--- week3/date_simple.py
+++ week3/date_simple.py
@@ -2,6 +2,7 @@ import datetime as dt
 
 
 def get_date(*argv):
+    """get_date(): takes an optional string and returns a date object"""
     if argv:
         str_date = str(*argv)
         date_patterns = ['%Y-%m-%d', '%d-%b-%y', '%d/%m/%Y']
@@ -12,3 +13,27 @@ def get_date(*argv):
                 pass
     else:
         return dt.datetime.now()
+
+
+def add_day(date_obj, *argv):
+    """add_day(): takes a date object and an optional integer and returns a date object"""
+    if argv:
+        time_delta = dt.timedelta(int(*argv))
+        new_date_obj = date_obj + time_delta
+    else:
+        time_delta = dt.timedelta(1)
+        new_date_obj = date_obj + time_delta
+    return new_date_obj
+
+
+def add_week(date_obj, *argv):
+    """add_week(): same as add_day() but adding days * 7"""
+    if argv:
+        time_delta = dt.timedelta(int(*argv))
+        week_delta = time_delta * 7
+        new_date_obj = date_obj + week_delta
+    else:
+        time_delta = dt.timedelta(1)
+        week_delta = time_delta * 7
+        new_date_obj = date_obj + week_delta
+    return new_date_obj
