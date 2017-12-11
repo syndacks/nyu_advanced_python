58c57cfb2a39f1e8b0611dbc7b03b0d72be18175
syndacks <dacks.milliken@slader.com>
2017-12-11 11:42:18 -0500

    Fix-ups for hw 3

--- week3/3-1_datetime.py
+++ week3/3-1_datetime.py
@@ -4,16 +4,19 @@ import date_simple as ds
 function either returns a datetime.date object (noted below as dt) or returns a
 string that represents a date. """
 
-# dt = ds.get_date('2016-05-05')
-dt = ds.get_date('5/5/2016')
-# dt = ds.get_date('5-May-16')
-# dt = ds.get_date()
+dt = ds.get_date('2016-05-05')
+# dt1 = ds.get_date('5/5/2016')
+# dt2 = ds.get_date('5-May-16')
+# dt3 = ds.get_date()
 
-print "dt", dt
+# print "dt", dt1
+# print "dt", dt2
+# print "dt", dt3
 
 # dt2 = ds.add_day(dt, -3)
-# dt2 = ds.add_day(dt)
+# dt3 = ds.add_day(dt)
 # print "dt2", dt2
+# print "dt3", dt3
 
 # dt3_add_week = ds.add_week(dt)
 # dt3_add_week = ds.add_week(dt,2)
@@ -21,5 +24,5 @@ print "dt", dt
 
 # dt_format = ds.format_date(dt, 'MM/DD/YYYY')
 # dt_format = ds.format_date(dt, 'DD-Mon-YY')
-# dt_format = ds.format_date(dt)
-# print "dt_format", dt_format
+dt_format = ds.format_date(dt)
+print "dt_format", dt_format
